7284719b98329043e2869bfc85dbd4888a7914e1
DeltaTerran <Banelingrevolution@gmail.com>
2022-11-24 02:53:01 +0200

    3.5
    У меня был баг при котором противник не выпускал Шары. Причина была в том что я забыл добавить скрипт по которому распознаётся персонаж к персонажу

---
+++ Unity v deistvii 2.2/Assets/Scripts CH-II/Fireball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Fireball : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public int damage = 1;
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+    //Эта функция вызывается, когда с триггером сталкивается другой объект.
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        //Проверяем, является ли этот другой объект объектом PlayerCharacter
+        if (player != null)
+        {
+            player.Hurt(damage);
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Unity v deistvii 2.2/Assets/Scripts CH-II/PlayerCharacter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerCharacter : MonoBehaviour
+{
+    private int _health; 
+    void Start()
+    {
+        // Инициализация переменной health.
+        _health = 5;
+    }
+    public void Hurt(int damage)
+    {
+        // Уменьшение здоровья игрока.
+        _health -= damage;
+        Debug.Log("Health: " + _health);
+    }
+}
