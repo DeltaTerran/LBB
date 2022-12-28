77272730294e7a76c9b888423e23a512eec6f92a
DeltaTerran <Banelingrevolution@gmail.com>
2022-12-29 01:19:26 +0200

    7.2
    Настройка GUI

---
+++ Unity v deistvii 2.2/Assets/Scripts CH-7/BasicUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnGUI()
+    {
+		if (GUI.Button(new Rect(10, 10, 40, 20), "Test"))
+        {
+			Debug.Log("Test Button");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
